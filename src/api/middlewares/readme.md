56981e0a481ee62b07b2dd5145924997968ce07e
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2023-10-31 14:55:45 +0800

    feat: added jwt middleware for private routes

--- src/api/middlewares/readme.md
+++
@@ -1,1 +0,0 @@
-# put all middlewares in this directory
---
+++ src/api/middlewares/verifyToken.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+exports.verifyToken = (req, res, next) => {
+	const token = req.header('auth-token');
+	if (!token) return res.status(401).json({ message: 'Access Denied' });
+
+	try {
+		const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+		req.user = verified;
+
+		next();
+	} catch (error) {
+		res.status(400).json({ message: 'Invalid Token' });
+	}
+};
